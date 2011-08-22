e0604a8e600edf15f8257f48787ae8a266617e3c
Andrew Buntine <bunts@hhd.com.au>
2011-08-22 13:36:53 +1000

    Shuffled planets, added correct maths

--- public/javascripts/jquery.planet.js
+++ public/javascripts/jquery.planet.js
@@ -5,26 +5,26 @@
 // Represents a planet in a solar-system. Part of a simple demonstration of the parallax effect.
 
 (function($){
-  $.fn.basketball = function(){
+  $.fn.planet = function(){
     return this.each(function(){
       var element = $(this);
       var adjuster = parseInt(element.attr("data-adjust"));
       var distance = parseFloat(element.attr("data-distance"));
       var threshold = 7306376272.0;
+      var threshold = 40000000.0;
       var window_height = $(window).height();
-
+      var speed = threshold / distance;
+console.log(speed);
+ 
       function newPos(x, height, pos, adjuster, inertia) {
         return x + "% " + (-((height + pos) - (height + adjuster)) * inertia)  + "px";
       } 
 
       $(window).scroll(function(){
-        if (element.parents("section").hasClass("active")) {
-          var current_pos = $(window).scrollTop();
-          var speed = distance / 7306376272.0;
-          var new_pos = newPos(element.attr("data-x"), window_height, current_pos, adjuster, speed);
+        var current_pos = $(window).scrollTop();
+        var new_pos = newPos(element.attr("data-x"), window_height, current_pos, adjuster, speed);
 
-          element.css("backgroundPosition", new_pos);
-        }
+        element.css("backgroundPosition", new_pos);
       });
     });
   };
